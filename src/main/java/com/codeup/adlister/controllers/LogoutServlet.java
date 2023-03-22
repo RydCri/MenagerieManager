175d8a6dc737ee1fb0a968c2b06e7d1a1c5fd4bc
RydCri <>
2023-03-22 13:35:54 -0700

    added redirect logic to send client to login page

--- src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LogoutServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,12 @@ import java.io.IOException;
 @WebServlet(name = "controllers.LogoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        User currentUser = (User) request.getSession().getAttribute("user");
+        if(currentUser != null){
         request.getRequestDispatcher("/WEB-INF/logout.jsp").forward(request,response);
+        }else {
+            response.sendRedirect("/login");
+        }
+
     }
 }
