a55984aa7e2cc5971f841f049a4145600ab66427
RydCri <>
2023-03-23 13:13:19 -0700

    redirects user to /ads instead of profile

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -35,6 +35,7 @@ public class LoginServlet extends HttpServlet {
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
+
             response.sendRedirect("/ads");
         } else {
             response.sendRedirect("/login");
