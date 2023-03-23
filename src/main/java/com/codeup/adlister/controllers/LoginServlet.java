1ab9e4246e997915686c68022957b502d633c134
RydCri <>
2023-03-23 16:25:26 -0700

    pushing to new repo

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -20,7 +20,6 @@ public class LoginServlet extends HttpServlet {
         }
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
